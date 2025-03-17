94f732b4d2ef47400f05b179fbbd288d10f114a9
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-03-17 22:34:49 +0530

    Update

--- react-course/src/Components/React Hooks/UseState.jsx
+++ react-course/src/Components/React Hooks/UseState.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 
 export default function UseState() {
     const [count, setcount] = useState(0);
+    
   return (
     <div className='app'>
+        <p> useState() :- Manages state in a functional component</p>
         <p>Count:{count}</p>
         <button onClick={()=>{
             setcount(count+1)
